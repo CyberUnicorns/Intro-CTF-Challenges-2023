c5ead69a10417813f247995ba4ad7319270c6a49
jojo23works <32582583+jojo23works@users.noreply.github.com>
2022-08-27 17:51:29 -0400

    Fixing a bug

--- c2/script.js
+++ c2/script.js
@@ -1,37 +1,30 @@
 window.addEventListener("load", main)
-function main()
-{
-    //get the last p
-    //set a timer
-    let p1 = document.createElement("p");
-    p1.innerHTML = "Try googling 'child page url'";
-    window.setTimeout(temp, 20000);
-    function temp(){
-        document.body.appendChild(p1)
+function main() {
+    const messages = ["Try googling 'child page url'", "Ever notice sites with stuff like funcoolsite.com/about/coolblogthing.html?", "Try /secret.html", "Patience is key with a CTF 😊"];
+    let p = document.createElement("p");
+    let i = 0;
+    p.innerHTML = messages[i];
+    let timer = window.setInterval(temp, 20000);
+    function temp() {
+        if (i < 4) {
+            p.innerHTML = messages[i++];
+            document.body.appendChild(p);
+            p = document.createElement("p");
+        }
+        if (i === 2) {
+            clearInterval(timer);
+            timer = window.setInterval(temp, 40000);
+        }
+        if (i === 3) {
+            clearInterval(timer);
+            timer = window.setInterval(temp, 60000);
+        }
+        if (i >= 4){
+            timer = window.setTimeout(go, 3000);
+        }       
     }
-    
-    let p2 = document.createElement("p");
-    p2.innerHTML = "Ever notice sites with stuff like funcoolsite.com/about/coolblogthing.html?";
-    window.setTimeout(temp, 20000);
-    function temp(){
-        document.body.appendChild(p2)
-    }
-    
-    let p3 = document.createElement("p");
-    p3.innerHTML = "Try /secret.html";
-    window.setTimeout(temp, 40000);
-    function temp(){
-        document.body.appendChild(p3)
-    }
-    
-    let p4 = document.createElement("p");
-    p4.innerHTML = "Patience is key with a CTF 😊";
-    window.setTimeout(temp, 60000);
-    window.setTimeout(go, 3000);
-    function temp(){
-        document.body.appendChild(p4)
-    }
-    function go(){
+    function go() {
         window.location.href = "https://cyberunicorns.github.io/Intro-CTF-Challenges-2023/c2/secret.html";
     }
+
 }
